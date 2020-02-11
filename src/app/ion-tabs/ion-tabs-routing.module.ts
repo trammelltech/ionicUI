383a56b3898c26677d9c4b0ebd53b9916fa8a548
trammelltech <trammelltech@icloud.com>
2020-02-10 19:24:32 -0600

    fix tabs

--- src/app/ion-tabs/ion-tabs-routing.module.ts
+++ src/app/ion-tabs/ion-tabs-routing.module.ts
@@ -3,55 +3,51 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { IonTabsPage } from './ion-tabs.page';
 
-const routes: Routes = [
-  {
-    path: 'ion-tabs',
-    component: IonTabsPage,
-    children:
-      [
-        {
-          path: 'tab1',
-          children:
-            [
+    const routes: Routes = [
+      {
+        path: 'ion-tabs',
+        component: IonTabsPage,
+        children: [
+          {
+            path: 'tab1',
+            children: [
               {
                 path: '',
                 loadChildren: '../tab1/tab1.module#Tab1PageModule'
               }
             ]
-        },
-        {
-          path: 'tab2',
-          children:
-            [
+          },
+          {
+            path: 'tab2',
+            children: [
               {
                 path: '',
                 loadChildren: '../tab2/tab2.module#Tab2PageModule'
               }
             ]
-        },
-        {
-          path: 'tab3',
-          children:
-            [
+          },
+          {
+            path: 'tab3',
+            children: [
               {
                 path: '',
                 loadChildren: '../tab3/tab3.module#Tab3PageModule'
               }
             ]
-        },
-        {
-          path: '',
-          redirectTo: 'tab1',
-          pathMatch: 'full'
-        }
-      ]
-  },
-  {
-    path: '',
-    redirectTo: 'ion-tabs',
-    pathMatch: 'full'
-  }
-];
+          },
+          {
+            path: '',
+            redirectTo: '/ion-tabs/tab1',
+            pathMatch: 'full'
+          }
+        ]
+      },
+      {
+        path: '',
+        redirectTo: '/ion-tabs/ion-tabs/tab1',
+        pathMatch: 'full'
+      }
+    ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
